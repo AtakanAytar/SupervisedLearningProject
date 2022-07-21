ab3856af0f0daf648bf6dbb455e44ab5a0824cd3
Jamaal Bernabe <jnbernabe@gmail.com>
2022-07-21 10:10:52 -0400

    Added Neural Network

--- source.py
+++ source.py
@@ -19,9 +19,6 @@ from sklearn.model_selection import train_test_split
 
 df_ksi = pd.read_csv ("./KSI.csv")
 
-
-
-
 ## Data Exploration 
 # Atakan Aytar
 print("First three records")
@@ -41,6 +38,7 @@ print("Non null count")
 print(df_ksi.info())
 
 #See coords of the accidents
+#See coords of the accidents
 df_ksi.plot(kind="scatter", x="X", y="Y")
 plt.title("Coords of Accidents")
 plt.xlabel("X")
@@ -53,8 +51,33 @@ plt.xlabel("DISTRICT")
 plt.ylabel("freqeuncy")
 plt.show()
 
+df_ksi["ROAD_CLASS"].hist(bins=30, figsize=(25,15))
+plt.xlabel("ROAD_CLASS")
+plt.ylabel("freqeuncy")
+plt.show()
+
+df_ksi["ACCLOC"].hist(bins=30, figsize=(25,15))
+plt.xlabel("ACCLOC")
+plt.ylabel("freqeuncy")
+plt.show()
+
+df_ksi["TRAFFCTL"].hist(bins=30, figsize=(25,15))
+plt.xlabel("TRAFFCTL")
+plt.ylabel("freqeuncy")
+plt.show()
+
+df_ksi["VISIBILITY"].hist(bins=30, figsize=(25,15))
+plt.xlabel("VISIBILITY")
+plt.ylabel("freqeuncy")
+plt.show()
+
 df_ksi["LIGHT"].hist(bins=30, figsize=(25,15))
 plt.xlabel("LIGHT")
+plt.ylabel("freqeuncy")
+plt.show()
+
+df_ksi["RDSFCOND"].hist(bins=30, figsize=(25,15))
+plt.xlabel("RDSFCOND")
 plt.ylabel("freqeuncy")
 plt.show()
 
@@ -63,10 +86,35 @@ plt.xlabel("ALCOHOL")
 plt.ylabel("freqeuncy")
 plt.show()
 
+df_ksi["ACCLASS"].hist(bins=30, figsize=(25,15))
+plt.xlabel("ACCLASS")
+plt.ylabel("freqeuncy")
+plt.show()
+
+df_ksi["IMPACTYPE"].hist(bins=40, figsize=(25,15))
+plt.xlabel("IMPACTYPE")
+plt.ylabel("freqeuncy")
+plt.show()
+
+ax= plt.subplot()
+df_ksi["INVTYPE"].hist(bins=30, figsize=(25,15))
+plt.xlabel("INVTYPE")
+plt.ylabel("freqeuncy")
+plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+plt.show()
+
+df_ksi["INVAGE"].hist(bins=50, figsize=(25,15))
+plt.xlabel("INVAGE")
+plt.ylabel("freqeuncy")
+plt.show()
+
+ax= plt.subplot()
 df_ksi["VEHTYPE"].hist(bins=40, figsize=(25,15))
 plt.xlabel("VEHTYPE")
 plt.ylabel("freqeuncy")
+plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
 plt.show()
+
 
 #See pairplot
 sns.pairplot(df_ksi)
@@ -76,6 +124,37 @@ plt.show()
 df_ksi.hist(bins=50, figsize=(20,15))
 plt.show()
 
+# Pie chart
+fig1, ax1 = plt.subplots()
+explode = [0,0.1,0]
+df_ksi.groupby('ACCLASS').size().plot(kind='pie', explode=explode, autopct='%.2f%%', ax=ax1)
+ax1.set_ylabel('Accident Class', size=16)
+ax1.axis('equal') 
+plt.show()
+
+fig1, ax1 = plt.subplots()
+df_ksi.groupby('IMPACTYPE').size().plot(kind='pie', autopct='%.2f%%', ax=ax1)
+ax1.set_ylabel('Impact Type', size=22)
+ax1.axis('equal') 
+plt.show()
+
+fig1, ax1 = plt.subplots()
+df_ksi.groupby('DISTRICT').size().plot(kind='pie', autopct='%.2f%%', ax=ax1)
+ax1.set_ylabel('District', size=16)
+ax1.axis('equal') 
+plt.show()
+
+fig1, ax1 = plt.subplots()
+df_ksi.groupby('ACCLOC').size().plot(kind='pie', autopct='%.2f%%', ax=ax1)
+ax1.set_ylabel('Collision Location', size=16)
+ax1.axis('equal') 
+plt.show()
+
+fig1, ax1 = plt.subplots()
+df_ksi.groupby('TRAFFCTL').size().plot(kind='pie', autopct='%.2f%%', ax=ax1)
+ax1.set_ylabel('Traffic Control Type', size=16)
+ax1.axis('equal') 
+plt.show()
 ## End of Data exploration
 
 
@@ -164,7 +243,7 @@ pre_processor = ColumnTransformer(
 )
 
 
-## The Model
+## The Models
 ##############
 
 ## LOGISTIC REGRESSION
@@ -209,6 +288,7 @@ pipeline3 = Pipeline(
 pipeline3.fit(dfksi_train_X, dfksi_train_y)
 print("Decision Tree model score: %.3f" % pipeline3.score(dfksi_test_X, dfksi_test_y))
 
+#SVC Adrian Posadas
 from sklearn.svm import SVC
 
 pipeline2 = Pipeline(
@@ -247,5 +327,63 @@ pipeline4 = Pipeline(
 pipeline4.fit(dfksi_train_X, dfksi_train_y)
 print("Random forest model score: %.3f" % pipeline4.score(dfksi_test_X, dfksi_test_y))
 
-
 #End of random forest
+
+#Neural Network Model - Jamaal Bernabe
+#import MLP Classifier
+print("Neural Network Model")
+from sklearn.neural_network import MLPClassifier
+mlp = MLPClassifier(random_state=123)
+pipe = Pipeline(steps=[('pre',pre_processor),('mlpc', mlp)])
+
+#GridSearch
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import confusion_matrix
+#Function to display metrics easily
+def scoremodel(model):
+    print("Training Accuracy: " , model.score(dfksi_train_X,dfksi_train_y))
+    print("Test Accuracy: " , model.score(dfksi_test_X,dfksi_test_y))
+    y_pred = model.predict(dfksi_test_X)
+    print("Accuracy Matrix: " , confusion_matrix(dfksi_test_y, y_pred))
+    print("Best Parameters: " ,model.best_params_)
+    print("Best Score: " ,model.best_score_)
+    
+param_grid = {
+            'mlpc__max_iter': [500,600],
+            'mlpc__hidden_layer_sizes': [(8,4,2),(4,2,1)],
+            'mlpc__activation': ['tanh', 'relu'],
+            'mlpc__solver': ['sgd', 'adam'],
+            'mlpc__alpha': [0.0001,0.001, 0.01],
+            'mlpc__learning_rate': ['constant','adaptive'],
+}
+
+rnd_grid = RandomizedSearchCV(estimator=pipe, param_distributions= param_grid, cv=3,n_jobs = -1,n_iter=10)
+rnd_result = rnd_grid.fit(dfksi_train_X,dfksi_train_y)
+scoremodel(rnd_grid)
+
+best_params = [
+    {'mlpc__activation': 'tanh', 'mlpc__alpha': 0.01, 'mlpc__hidden_layer_sizes': (8, 4, 2), 'mlpc__learning_rate': 'constant', 'mlpc__max_iter': 600, 'mlpc__solver': 'adam'},
+    {'mlpc__activation': 'tanh', 'mlpc__alpha': 0.01, 'mlpc__hidden_layer_sizes': (4, 2, 1), 'mlpc__learning_rate': 'constant', 'mlpc__max_iter': 600, 'mlpc__solver': 'adam'},
+    {'mlpc__activation': 'tanh', 'mlpc__alpha': 0.01, 'mlpc__hidden_layer_sizes': (8, 4, 2), 'mlpc__learning_rate': 'adaptive', 'mlpc__max_iter': 600, 'mlpc__solver': 'adam'},
+    {'mlpc__activation': 'tanh', 'mlpc__alpha': 0.01, 'mlpc__hidden_layer_sizes': (4, 2, 1), 'mlpc__learning_rate': 'adaptive', 'mlpc__max_iter': 600, 'mlpc__solver': 'adam'}
+    ]
+#5 minutes
+grid = GridSearchCV(estimator=pipe, param_grid=param_grid, cv=3, n_jobs = -1,scoring="accuracy",refit=True)
+grid_result = grid.fit(dfksi_train_X,dfksi_train_y)
+y_pred = grid_result.predict(dfksi_test_X)
+scoremodel(grid_result)
+
+#Graph
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn import metrics
+
+ConfusionMatrixDisplay.from_estimator(grid_result,dfksi_train_X,dfksi_train_y)
+ConfusionMatrixDisplay.from_predictions(y_pred,dfksi_test_y)
+
+fpr, tpr, thresholds = metrics.roc_curve(y_pred,dfksi_test_y, y_pred)
+roc_auc = metrics.auc(fpr, tpr)
+display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc,
+                               estimator_name='Neural Network')
+display.plot()
+plt.show()
