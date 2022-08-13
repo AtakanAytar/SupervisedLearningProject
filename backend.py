70efbb385e636196152ae26dbbafd29fb0571e0d
Jamaal Bernabe <jnbernabe@gmail.com>
2022-08-13 14:07:50 -0400

    All Routes functional in receiveing JSON. Response Fatal/NonFatal Need to determine adding metrics to response

--- backend.py
+++ backend.py
@@ -15,16 +15,118 @@ def main():
         
         # Unpickle classifier
         rf = joblib.load("randomforest.pkl")
-        
-
-        ###we have to get the values from the front end 
-        # Get values through input bars
-        height = request.form.get("height")
-        weight = request.form.get("weight")
-        # Put inputs to dataframe
-        X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
+        ##we have to get the values from the front end 
+        input = request.get_json()
+        x_dict = {}
+        for ikey in input:
+            x_dict[ikey] = pd.Series([input[ikey]])
+        X = pd.DataFrame(x_dict)
         # Get prediction
         prediction = rf.predict(X)[0]
+        #prediction = rf.score(X)[0]
+    else:
+        prediction = ""
+    ###return a json here   
+    return jsonify(prediction)
+
+@app.route('/forest', methods=['GET', 'POST'])
+def get_forest():
+    prediction = {}
+    # If a form is submitted
+    if request.method == "POST":
+        
+        # Unpickle classifier
+        rf = joblib.load("randomforest.pkl")
+       ###we have to get the values from the front end 
+        input = request.get_json()
+        x_dict = {}
+        for ikey in input:
+            x_dict[ikey] = pd.Series([input[ikey]])
+        X = pd.DataFrame(x_dict)
+        # Get prediction
+        prediction = rf.predict(X)[0]
+        #prediction = rf.score(X)[0]
+    else:
+        prediction = ""
+    ###return a json here   
+    return jsonify(prediction)
+
+#I think we should either have a route for each of these models or send something to Main fn
+@app.route("/nn", methods = ["GET","POST"])
+def get_nn():
+    prediction = {}
+    if request.method == "POST":
+        # Unpickle classifier
+        rf = joblib.load("./NeuralNetwork.pkl")
+        ###we have to get the values from the front end 
+        input = request.get_json()
+        x_dict = {}
+        for ikey in input:
+            x_dict[ikey] = pd.Series([input[ikey]])
+        X = pd.DataFrame(x_dict)
+        # Get prediction
+        prediction = rf.predict(X)[0]
+        #prediction = rf.score(X)[0]
+    else:
+        prediction = ""
+    ###return a json here   
+    return jsonify(prediction)
+
+@app.route("/tree", methods = ["GET","POST"])
+def get_tree():
+    prediction = {}
+    if request.method == "POST":
+        # Unpickle classifier
+        rf = joblib.load("DecTree.pkl")
+        ##we have to get the values from the front end 
+        input = request.get_json()
+        x_dict = {}
+        for ikey in input:
+            x_dict[ikey] = pd.Series([input[ikey]])
+        X = pd.DataFrame(x_dict)
+        # Get prediction
+        prediction = rf.predict(X)[0]
+        #prediction = rf.score(X)[0]
+    else:
+        prediction = ""
+    ###return a json here   
+    return jsonify(prediction)
+
+@app.route("/logreg", methods = ["GET","POST"])
+def get_log():
+    prediction = {}
+    if request.method == "POST":
+        # Unpickle classifier
+        rf = joblib.load("LogReg.pkl")
+        ##we have to get the values from the front end 
+        input = request.get_json()
+        x_dict = {}
+        for ikey in input:
+            x_dict[ikey] = pd.Series([input[ikey]])
+        X = pd.DataFrame(x_dict)
+        # Get prediction
+        prediction = rf.predict(X)[0]
+        #prediction = rf.score(X)[0]
+    else:
+        prediction = ""
+    ###return a json here   
+    return jsonify(prediction)
+
+@app.route("/svm", methods = ["GET","POST"])
+def get_svm():
+    prediction = {}
+    if request.method == "POST":
+        # Unpickle classifier
+        rf = joblib.load("SVM.pkl")
+        ##we have to get the values from the front end 
+        input = request.get_json()
+        x_dict = {}
+        for ikey in input:
+            x_dict[ikey] = pd.Series([input[ikey]])
+        X = pd.DataFrame(x_dict)
+        # Get prediction
+        prediction = rf.predict(X)[0]
+        #prediction = rf.score(X)[0]
     else:
         prediction = ""
     ###return a json here   
@@ -35,116 +137,6 @@ def test():
     if request.method == "POST":
         #print(jsonify(request.data))
         return request.get_json()
-        
-
-#I think we should either have a route for each of these models or send something to Main fn
-@app.route("/nn", methods = ["GET","POST"])
-def get_nn():
-    prediction = {}
-    
-    if request.method == "POST":
-        # Unpickle classifier
-        #rf = joblib.load("./NeuralNetwork.pkl")
-        #req_json = request.get_json(force=True)
-        #req_json = jsonify(req_json)
-        #print(request.form)
-        ###we have to get the values from the front end 
-        
-        test_json = {"HOUR":20,"TIME":2038,"STREET1":"FINCH Ave W","STREET2":"DUFFERIN St","DISTRICT":"North York","HOOD_ID":27,"TRAFFCTL":"No Control","VISIBILITY":"Rain","LIGHT":"Dark","RDSFCOND":"Wet","IMPACTYPE":"Pedestrian Collisions","INVTYPE":"Driver","INVAGE":"15 to 19","VEHTYPE":"Automobile, Station Wagon","LONGITUDE":-79.46989,"LATITUDE":43.768145}
-        #json to dataframe
-        columnss = ['HOUR', 'TIME', 'STREET1', 'STREET2', 'DISTRICT', 'HOOD_ID', 'TRAFFCTL',
-       'VISIBILITY', 'LIGHT', 'RDSFCOND', 'IMPACTYPE', 'INVTYPE', 'INVAGE',
-       'VEHTYPE', 'LONGITUDE', 'LATITUDE']
-
-        x_dict = {}
-        for ikey in test_json:
-            x_dict[ikey] = pd.Series([test_json[ikey]])
-
-        X = pd.DataFrame(x_dict)
-        
-        #test_json = jsonify(test_json)
-        #X= pd.DataFrame(test_json.values(), columns = test_json.keys())
-        # X = pd.read_json(test_json)
-        print(X)
-    
-
-        # return jsonify(x_dict)
-        # Get prediction
-        #prediction = rf.predict(X)[0]
-        
-    else:
-        prediction = ""
-    ###return a json here   
-    return jsonify(prediction)
-
-@app.route("/tree", methods = ["GET","POST"])
-def get_tree():
-    if request.method == "Decision Tree Model":
-        # Unpickle classifier
-        rf = joblib.load("DecTree.pkl")
-
-        ###we have to get the values from the front end 
-
-        # Get values through input bars
-        height = request.form.get("height")
-        weight = request.form.get("weight")
-        
-        # Put inputs to dataframe
-        X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
-        
-        # Get prediction
-        prediction = rf.predict(X)[0]
-        
-    else:
-        prediction = ""
-    ###return a json here   
-    return jsonify(prediction)
-
-@app.route("/logreg", methods = ["GET","POST"])
-def get_log():
-    if request.method == "LogisticRegression Model":
-        # Unpickle classifier
-        rf = joblib.load("LogReg.pkl")
-
-        ###we have to get the values from the front end 
-
-        # Get values through input bars
-        height = request.form.get("height")
-        weight = request.form.get("weight")
-        
-        # Put inputs to dataframe
-        X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
-        
-        # Get prediction
-        prediction = rf.predict(X)[0]
-        
-    else:
-        prediction = ""
-    ###return a json here   
-    return jsonify(prediction)
-
-@app.route("/svm", methods = ["GET","POST"])
-def get_svm():
-    if request.method == "SVM Model":
-        # Unpickle classifier
-        rf = joblib.load("SVM.pkl")
-
-        ###we have to get the values from the front end 
-
-        # Get values through input bars
-        height = request.form.get("height")
-        weight = request.form.get("weight")
-        
-        # Put inputs to dataframe
-        X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
-        
-        # Get prediction
-        prediction = rf.predict(X)[0]
-        
-    else:
-        prediction = ""
-    ###return a json here   
-    return jsonify(prediction)
 
 # Running the app
 if __name__ == '__main__':
